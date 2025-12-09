b81129370a2dfe3d3a845906817991481361240c
Christian <christiansamuelsribeiro@gmail.com>
2025-12-08 23:20:33 -0300

    remontando

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-
-<footer>
-    <p>© 2025 BarberKush - Todos os direitos reservados.</p>
-</footer>
-
-</body>
-</html>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include "includes/header.php"; ?>
-
-<h2>Bem-vindo ao Sistema BarberKush</h2>
-<p>Gerencie clientes, serviços, profissionais e agendamentos de forma simples.</p>
-
-<?php include "includes/footer.php"; ?>
