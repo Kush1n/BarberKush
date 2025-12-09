c0b970d69f49cab3633aeb8b2efde7012ec07005
Kush1n <123014752+Kush1n@users.noreply.github.com>
2025-12-08 23:21:56 -0300

    remontando database

--- Mysql.sql
+++ Mysql.sql
@@ -1,40 +1,43 @@
+
 CREATE DATABASE barberkush;
 USE barberkush;
 
 CREATE TABLE cliente (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
-    email VARCHAR(100),
-    data_cadastro DATE NOT NULL
+    telefone VARCHAR(20) NOT NULL UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE profissional (
-    id_profissional INT AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE barbeiro (
+    id_barbeiro INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    especialidade VARCHAR(100),
     telefone VARCHAR(20),
-    status ENUM('ativo', 'inativo') DEFAULT 'ativo'
+    especialidade VARCHAR(100),
+    status ENUM('ativo','inativo') DEFAULT 'ativo'
 );
 
 CREATE TABLE servico (
     id_servico INT AUTO_INCREMENT PRIMARY KEY,
-    nome_servico VARCHAR(100) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
     descricao TEXT,
-    valor DECIMAL(10,2) NOT NULL CHECK (valor > 0),
-    tempo_estimado INT NOT NULL
+    valor DECIMAL(10,2) NOT NULL,
+    duracao INT NOT NULL -- duração em minutos
 );
 
 CREATE TABLE agendamento (
     id_agendamento INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT NOT NULL,
-    id_profissional INT NOT NULL,
+    id_barbeiro INT NOT NULL,
     id_servico INT NOT NULL,
     data DATE NOT NULL,
     hora TIME NOT NULL,
-    status ENUM('pendente', 'concluido', 'cancelado') DEFAULT 'pendente',
+    status ENUM('agendado','concluido','cancelado') DEFAULT 'agendado',
+    valor_total DECIMAL(10,2),
 
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-    FOREIGN KEY (id_profissional) REFERENCES profissional(id_profissional),
+    FOREIGN KEY (id_barbeiro) REFERENCES barbeiro(id_barbeiro),
     FOREIGN KEY (id_servico) REFERENCES servico(id_servico)
 );
 
@@ -42,8 +45,8 @@ CREATE TABLE pagamento (
     id_pagamento INT AUTO_INCREMENT PRIMARY KEY,
     id_agendamento INT NOT NULL,
     valor DECIMAL(10,2) NOT NULL,
-    forma_pagamento ENUM('pix', 'cartao', 'dinheiro') NOT NULL,
-    data_pagamento DATE NOT NULL,
-
+    forma_pagamento ENUM('dinheiro','cartao','pix') NOT NULL,
+    data_pagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
     FOREIGN KEY (id_agendamento) REFERENCES agendamento(id_agendamento)
 );
